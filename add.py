4eec7afbe5847cdb69f731f2b70f4d21ec25857a
bipin2324 <168274598+bipin2324@users.noreply.github.com>
2024-04-27 19:50:52 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,20 @@
+#program to add to numbers
+
+a = 3;
+b = 5;
+c = 0;
+
+def sum(a,b):
+   """
+adding 2 numbers
+   :param a:
+   :param b:
+   :return:
+   """
+   c = a + b;
+   return c;
+
+d=sum(a,b)
+print ("value of a is :", a )
+print ("value of b is :", b )
+print ("sum of a+b  is :", d )
